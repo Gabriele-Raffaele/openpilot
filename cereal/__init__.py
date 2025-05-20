4082c8baaa08b1a83b97391a9459772f9ad451aa
Gabriele-Raffaele <gabriele.raffaele87@gmail.com>
2025-05-20 22:36:55 +0200

    system/ui: add debug prints for current working directory and CEREAL_PATH

--- cereal/__init__.py
+++ cereal/__init__.py
@@ -3,9 +3,10 @@ import capnp
 from importlib.resources import as_file, files
 
 capnp.remove_import_hook()
-
+print("position:", os.getcwd())
 with as_file(files("cereal")) as fspath:
   CEREAL_PATH = fspath.as_posix()
+  print(f"CEREAL_PATH: {CEREAL_PATH}")
   log = capnp.load(os.path.join(CEREAL_PATH, "log.capnp"))
   car = capnp.load(os.path.join(CEREAL_PATH, "car.capnp"))
   custom = capnp.load(os.path.join(CEREAL_PATH, "custom.capnp"))
